fd20d592d4821780f804f0e30a9c047f61f54f44
Philippe Henry <philippe.henry@owse.com.br>
2025-04-09 15:22:13 -0300

    Commit das Issues #10 #11 #12

---
+++ api/wegia/app/Http/Controllers/EspecieController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Especie;
+
+class EspecieController extends Controller
+{
+    public function index()
+    {
+        return response()->json(Especie::all());
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'nome' => 'required|string|max:255',
+        ]);
+
+        $especie = Especie::create([
+            'nome' => $request->nome
+        ]);
+
+        return response()->json($especie, 201);
+    }
+
+    public function update(Request $request, $id)
+    {
+        $especie = Especie::findOrFail($id);
+
+        $request->validate([
+            'nome' => 'required|string|max:255',
+        ]);
+
+        $especie->update([
+            'nome' => $request->nome
+        ]);
+
+        return response()->json($especie);
+    }
+}
